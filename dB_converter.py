7b4620dcf453b195d14fa89295f5958696cec21f
jdwatson <j.d.watson@tudelft.nl>
2017-07-24 21:32:20 +0200

    Upload dBm converter

--- dB_converter.py
+++ dB_converter.py
@@ -9,4 +9,9 @@ V = np.sqrt(P*50) #[V] corresponding voltage for a 50 Ohm system
 
 print(str(dBm)+' dBm = '+str(V)+' V')
 
+V = 2.25
+P = V**2/50
+
+dBm = 10*np.log10(P/1e-3)
+print(str(V)+' V = '+str(dBm)+' dBm')
 #dB = 10*np.log10(P1/P2)
